39e2c3e7a8ce94e8434abf4dc6398bc2a5c7c599
hisaju <hisaju@gmail.com>
2024-09-24 19:14:51 +0900

    一旦コミット

--- Gemfile
+++ Gemfile
@@ -6,3 +6,5 @@ gem "sinatra", "~> 4.0"
 gem "sinatra-contrib", "~> 4.0"
 gem "puma", "~> 6.4"
 gem "dotenv", "~> 3.1"
+
+gem "faraday", "~> 2.9"
--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'uri'
+require 'net/http'
+require 'faraday'
 
 class App < Sinatra::Base
   configure :development do
@@ -7,6 +10,25 @@ class App < Sinatra::Base
   end
 
   get '/' do
+    options = {
+      url: 'https://api.publicapis.org'
+    }
+    conn = Faraday.new(options) do |f|
+      f.response :json
+      f.adapter :net_http
+      f.request :url_encoded
+    end
+
+    res = conn.get('/categories')
+    @categories = res.body['categories']
+
+    req_params = {
+    }
+    req_params[:category] = params['category'] if params['category']
+    req_params[:title] = params['title'] if params['title']
+
+    res = conn.get('/entries', req_params)
+    @list = res.body
     erb :index
   end
 end
